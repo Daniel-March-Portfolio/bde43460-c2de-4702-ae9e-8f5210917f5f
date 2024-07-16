6e5782159b26859bf9857fd6ad408a5d0604a9fa
Daniel-March <town.kor@gmail.com>
2024-07-16 16:43:50 +0300

    Added receipts calculating for any month

--- app/Receipt/api_methods.py
+++ app/Receipt/api_methods.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
@@ -10,6 +12,10 @@ def run_receipts_calculating(request):
         return HttpResponse(status=405)
     if not request.user.is_authenticated:
         return HttpResponse(status=403)
+    current_data = date.today()
+    month = request.GET.get("month") or current_data.month
+    year = request.GET.get("year") or current_data.year
+    date_for_calculating = date(year=int(year), month=int(month), day=1)
     progress_uuid = Progress.objects.create(target_value=1).uuid
-    Command().handle(progress_uuid=progress_uuid)
+    Command().handle(progress_uuid=progress_uuid, date_for_calculating=date_for_calculating)
     return redirect(f"/api/progress/{progress_uuid}")
--- app/Receipt/management/commands/calculate_receipts.py
+++ app/Receipt/management/commands/calculate_receipts.py
@@ -1,7 +1,6 @@
 from datetime import date
 from uuid import UUID
 
-from celery import shared_task
 from django.core.management.base import BaseCommand
 
 from Apartment.models import Apartment
@@ -12,8 +11,8 @@ from Receipt.tasks import calculate_receipt_for_apartments_with_celery
 class Command(BaseCommand):
     help = "Calculate receipts for all apartments"
 
-    def handle(self, *args, progress_uuid: UUID = None, **options):
-        current_date = date.today()
+    def handle(self, *args, progress_uuid: UUID = None, date_for_calculating: date = None, **options):
+        current_date = date_for_calculating or date.today()
         if progress_uuid is None:
             progress_uuid = Progress.objects.create(
                 target_value=Apartment.objects.count()
@@ -23,4 +22,3 @@ class Command(BaseCommand):
         calculate_receipt_for_apartments_with_celery.delay(
             progress_uuid=progress_uuid, month=current_date.month - 1, year=current_date.year
         )
-
